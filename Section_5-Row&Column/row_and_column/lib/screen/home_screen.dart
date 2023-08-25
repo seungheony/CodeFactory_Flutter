9a54e589a3a8889f4facc6e676759fefd8def406
김승헌 <shkim@nshc.net>
2023-08-25 16:27:06 +0900

    CrossAxisAlignment 완전정복

--- Section_5-Row&Column/row_and_column/lib/screen/home_screen.dart
+++ Section_5-Row&Column/row_and_column/lib/screen/home_screen.dart
@@ -10,6 +10,8 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
+          // MedeiaQuery.of로 size를 가져올 수 있다.
+          height: MediaQuery.of(context).size.height,
           child: Row(
             // MainAxisAlignment: 주축 정렬
             // start - 시작
@@ -18,7 +20,13 @@ class HomeScreen extends StatelessWidget {
             // spaceBetween - 위젯과 위젯의 사이가 동일하게 배치된다.
             // spaceEvenly - 위젯을 같은 간격으로 배치하지만 끝과 끝에도 위젯이 아닌 빈 간격으로 시작한다.
             // spaceAround - spaceEvenly + 끝과 끝의 간격은 1/2
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.start,
+            // CrossAxisAlignment - 반대축 정렬
+            crossAxisAlignment: CrossAxisAlignment.end,
+            // start - 시작
+            // end - 끝
+            // center - 중앙
+            // stretch - 하드코딩한 width와 상관 없이 최대한으로 늘린다.
             children: [
               Container(
                 color: Colors.red,
